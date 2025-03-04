cab31be253f21fbc2537847438ec297a9cf25da6
@max.naumanen <max.naumanen@tbwa.fi>
2025-03-04 08:47:28 +0200

    tailwind v4 does not work

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center">Yolo</div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="pt-32 md:pt-96">
+          <h1>ADEOLA</h1>
+        </div>
+      </div>
     </>
   );
 }
