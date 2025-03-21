9a38d6a9f1379a01e4aaa113ed5a7b41f9b1b187
adeolaikuesan <adeola.ikuesan@gmail.com>
2025-03-21 11:11:01 +0200

    About and footer components baseline

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import About from "./components/about";
+
 export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center">
         <div className="pt-32 md:pt-96">
-          <h1>ADEOLA</h1>
+          <About />
         </div>
       </div>
     </>
